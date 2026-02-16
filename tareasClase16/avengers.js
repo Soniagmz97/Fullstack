394fa6a76aab8d251b4e1b739fb7876c43180a61
Sonia Gomez <soniagmz@alumnos.cei.es>
2026-02-16 20:21:25 +0100

    cambios ejercicios

--- tareasClase16/avengers.js
+++ tareasClase16/avengers.js
@@ -9,18 +9,18 @@ const heroes = [
     { nombre: "Batman", nivel: 4, equipo: "Justice League" },
     { nombre: "Thor", nivel: 8, equipo: "Avengers" },
     { nombre: "Spider-Man", nivel: 3, equipo: "Avengers" }
-];
+]
 
 // Crea una función que filtre y devuelva en un nuevo array solo a los héroes que pertenecen al equipo "Avengers".
 // Crea otra función llamada subirNivel que reciba el nombre de un héroe y aumente su nivel en +1.Muestra el array final en la consola para verificar los cambios.
 
 // Debes subirlo a un proyecto en tu Github (aprovechen para usar ramas)
 
-const heros = () =>{
+const heros = () => {
     let arrayAvengers = []
 
-    heroes.forEach(( eachHero, index ) => {
-        if (eachHero.equipo === `Avengers`){
+    heroes.forEach((eachHero, index) => {
+        if (eachHero.equipo === `Avengers`) {
             arrayAvengers.push(eachHero)
         }
     })
@@ -29,8 +29,8 @@ const heros = () =>{
 }
 
 const subirNivel = (nombreHeroe) => {
-    heroes.forEach(( eachHero, index ) => {
-        if (eachHero.nombre === nombreHeroe){
+    heroes.forEach((eachHero, index) => {
+        if (eachHero.nombre === nombreHeroe) {
             eachHero.nivel += 1
         }
     })
