d4b6ec31361f623b85d5083e6fe5c2f0bde3ab15
Sonia Gomez <soniagmz@alumnos.cei.es>
2026-02-24 21:12:50 +0100

    ejercicio Reto 1 clase 21

---
+++ clase21/ejercicioReto1/index.ts
@@ -0,0 +1,32 @@
+// Ejercicio El Contrato Básico (Propiedades Opcionales)
+// Objetivo: Que aprendan a definir la forma de un objeto y entiendan que en la vida real, no todos los datos vienen siempre de la base de datos (uso del modificador opcional ?).
+// Contexto para los alumnos: Estás construyendo el perfil de usuario para una red social. Todos los usuarios tienen un username, un email y una edad. Sin embargo, la biografia y el telefono son campos que el usuario puede elegir no llenar.
+// Instrucciones:
+// Crea una interfaz llamada PerfilUsuario con las propiedades mencionadas.
+// Crea un objeto usuario1 que tenga todos los datos.
+// Crea un objeto usuario2 que solo tenga los datos obligatorios.
+
+interface perfilUsuario {
+    userName:string,
+    email:string,
+    age:number
+    biography?:string,
+    phoneNumber?:number
+}
+
+let usuario1:perfilUsuario = {
+    userName: `Usuario_1`,
+    email: `usuario1@algo.com`,
+    age: 22,
+    biography: `Estudiante de Informática en la universidad.`,
+    phoneNumber: 541023658
+}
+
+let usuario2:perfilUsuario = {
+    userName: `Usuario_2`,
+    email: `usuario2@algo.com`,
+    age: 50,
+}
+
+console.log(usuario1)
+console.log(usuario2)
--- clase21/ejercicioTypescript2/index.ts
+++ clase21/ejercicioTypescript2/index.ts
@@ -1,9 +1,21 @@
-const nombreOne:string = `Bruno Díaz`
-const artesMarcialesUno:string[] = [`Karate`, `Aikido`, `Wing Chun`, `Jiu-Jitsu`]
+// const nombreOne:string = `Bruno Díaz`
+// const artesMarcialesUno:string[] = [`Karate`, `Aikido`, `Wing Chun`, `Jiu-Jitsu`]
 
-const batman : {nombre:string, artesMarciales:string[]} = {
-    nombre: nombreOne,
-    artesMarciales: artesMarcialesUno
+// const batman : {nombre:string, artesMarciales:string[]} = {
+//     nombre: nombreOne,
+//     artesMarciales: artesMarcialesUno
+// }
+
+// console.log(batman)
+
+interface JusticeLeague {
+    nameJusticeLeague: string,
+    marcialArts:string[]
+}
+
+let batman:JusticeLeague = {
+    nameJusticeLeague : `Bruno Díaz`,
+    marcialArts : [`Karate`, `Aikido`, `Wing Chun`, `Jiu-Jitsu`]
 }
 
 console.log(batman)
