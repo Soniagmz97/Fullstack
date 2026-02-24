d58da851f4934d0de223c1074c94d20ef1e647a1
Sonia Gomez <soniagmz@alumnos.cei.es>
2026-02-24 20:12:14 +0100

    correcciones ejercicio Typescript 1 clase 21

--- clase21/ejercicioTypescript1/index.ts
+++ clase21/ejercicioTypescript1/index.ts
@@ -1,8 +1,8 @@
 let nombre:string = `Alberto Rojas`
 let edad:number = 38
-const PERSONAJE = {
-    nombre : nombre,
-    edad : edad
+const PERSONAJE : {nombre:string, edad:number} = {
+    nombre: nombre,
+    edad: edad
 }
 
 console.log(PERSONAJE)
