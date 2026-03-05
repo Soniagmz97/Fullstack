33cc92661e8a0b18a9933aaf6b9fee906507b632
Sonia Gomez <soniagmz@alumnos.cei.es>
2026-03-05 21:23:10 +0100

    ejercicio clase 27

--- clase26/src/App.tsx
+++ clase26/src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Interruptor from './components/Interruptor'
+import ElPortero from './components/ElPortero'
+import PassValidator from './components/PassValidator'
 
 function App() {
 
   return (
     <>
       <Interruptor />
+      <ElPortero />
+      <PassValidator />
     </>
   )
 }
